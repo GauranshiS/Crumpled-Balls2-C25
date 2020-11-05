27e0a060fd2b94332872ea414d8a02a1c9edda82
GauranshiS <72340133+GauranshiS@users.noreply.github.com>
2020-11-05 10:06:42 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,46 @@
+class Paper{
+
+	constructor(x,y,radius){
+	
+		var options={
+			isStatic:true,
+			restitution:0.3,
+			friction: 0.5,
+			density:1.2,
+
+		}
+	
+					this.radius=radius;
+				this.x=x;
+				this.y=y;
+				this.image=loadImage("Sprites1/crumpledPaperImage");
+                this.image=addImage("PaperImgge.png")
+
+				this.body=Bodies.circle(this.x,this.y,this.radius,options);
+				World.add(world,this.body);
+				
+				
+	
+	}
+	
+	display(){
+	
+	
+	
+	push ()
+	
+			translate(this.body.position.x,this.body.position.y)
+			ellipse(0,0,this.radius,this.radius);
+			
+			rotate (this.body.angle);
+	
+	pop ();
+	
+	
+	
+	
+	}
+	
+	
+		
+	}
